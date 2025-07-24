575e09be5143e95c9ecb0ee53451a45540c5fed6
Lenon69 <fobix.lol@gmail.com>
2025-07-24 11:42:14 +0200

    Added Google Tag Manager

--- src/components/layout.rs
+++ src/components/layout.rs
@@ -18,6 +18,18 @@ pub fn base_layout(
         (DOCTYPE)
         html lang="pl" class="scroll-smooth" {
             head {
+
+
+                script {
+                    (maud::PreEscaped(r#"
+                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','GTM-N6HHSWLP');
+                    "#))
+                }
+
                 // Cała zawartość <head> pozostaje bez zmian
                 meta charset="UTF-8";
                 meta name="viewport" content="width=device-width, initial-scale=1.0";
@@ -128,6 +140,14 @@ pub fn base_layout(
                 "
                 class="bg-brand-dark text-slate-200 antialiased"
             {
+
+                noscript {
+                    iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-N6HHSWLP"
+                        height="0"
+                        width="0"
+                        style="display:none;visibility:hidden" {}
+                }
                 // Pasek postępu (bez zmian)
                 div class="fixed top-0 left-0 h-1 bg-brand-cyan z-[99]" x-bind:style="`width: ${width}%`" {}
 
