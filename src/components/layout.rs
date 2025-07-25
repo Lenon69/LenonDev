7a4e987e11820146159cad4f9d3b838b4673fd41
Lenon69 <fobix.lol@gmail.com>
2025-07-25 18:13:14 +0200

    Little fixes

--- src/components/layout.rs
+++ src/components/layout.rs
@@ -20,6 +20,8 @@ pub fn base_layout(
         (DOCTYPE)
         html lang="pl" class="scroll-smooth" {
             head {
+                meta charset="UTF-8";
+                meta name="viewport" content="width=device-width, initial-scale=1.0";
                 script {
                     (maud::PreEscaped(r#"
                         const loadGtm = () => {
@@ -49,8 +51,6 @@ pub fn base_layout(
                 }
 
                 // Cała zawartość <head> pozostaje bez zmian
-                meta charset="UTF-8";
-                meta name="viewport" content="width=device-width, initial-scale=1.0";
                 title { (title) }
                 meta name="description" content=(meta_description);
                 meta name="keywords" content="tworzenie stron internetowych, strony internetowe na zamówienie, Rust web developer, Axum backend, HTMX frontend, szybkie strony, nowoczesne strony, programista Rust, portfolio programisty, responsywne strony, SEO strony, tani web developer, strony z HTMX, web design, strony dla firm, landing page, strona wizytówka, programowanie stron, zakup strony internetowej, strony pod SEO, profesjonalne strony internetowe, web development Rust, szybkie ładowanie stron, nowoczesne technologie webowe, strony internetowe Złoczew, Złoczew, Sieradz, Wieluń, tworzenie stron internetowych";
