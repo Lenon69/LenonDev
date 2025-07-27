9f2ec9611242cc85bfbf85fca57e36a61e3b43df
Lenon69 <fobix.lol@gmail.com>
2025-07-27 12:07:54 +0200

    Change Tailwind place in idx/layout

--- src/components/layout.rs
+++ src/components/layout.rs
@@ -22,7 +22,6 @@ pub fn base_layout(
             head {
                 meta charset="UTF-8";
                 meta name="viewport" content="width=device-width, initial-scale=1.0";
-                script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4?plugins=typography" {}
 
                 script {
                     (maud::PreEscaped(r#"
@@ -88,6 +87,7 @@ pub fn base_layout(
                 }
 
                 // --- KONIEC SEKCJI SEO I FAVICON ---
+                script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4?plugins=typography" {}
                 script defer src="https://cdn.jsdelivr.net/npm/htmx.org@2.0.6/dist/htmx.min.js" {}
 
                 script {
