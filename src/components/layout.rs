45567182824e9cd2d7312c3226c6dacb9cc89dcb
Lenon69 <fobix.lol@gmail.com>
2025-07-27 12:23:00 +0200

    Added preconnect for cdn

--- src/components/layout.rs
+++ src/components/layout.rs
@@ -22,7 +22,7 @@ pub fn base_layout(
             head {
                 meta charset="UTF-8";
                 meta name="viewport" content="width=device-width, initial-scale=1.0";
-
+                link rel="preconnect" href="https://cdn.jsdelivr.net";
                 script {
                     (maud::PreEscaped(r#"
                         const loadGtm = () => {
